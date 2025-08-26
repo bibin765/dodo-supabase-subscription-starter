2ac4da18612a7f39e7dbf8982c3fa27b27e82755
Darshan Gowda <96766940+darshansrc@users.noreply.github.com>
2025-08-26 19:47:49 +0530

    chore: minor fixes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./drizzle/schema.ts",
-  out: "./drizzle/migrations",
+  schema: "./lib/drizzle/schema.ts",
+  out: "./lib/drizzle/migrations",
   dialect: "postgresql",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
